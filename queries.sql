191fe2915d28dd370bf9ea739f9550a76d42d4ab
Juan Diaz <jfdiaza@unal.edu.co>
2022-10-18 12:05:34 -0400

    add required transactions

--- queries.sql
+++ queries.sql
@@ -7,4 +7,35 @@ SELECT * from animals WHERE name IN ('Agumon','Pikachu');
 SELECT name, escape_attempts from animals WHERE weight_kg < 10.5;
 SELECT * from animals WHERE neutered = 'TRUE';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-SELECT * from animals WHERE name <> 'Gabumon';
+
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon'
+WHERE name NOT LIKE '%mon';
+COMMIT;
+
+BEGIN;
+DELETE from animals;
+ROLLBACK;
+
+BEGIN;
+DELETE from animals WHERE date_of_birth > '2022/01/01';
+SAVEPOINT savepoint_01;
+UPDATE animals
+SET weight_kg = weight_kg*-1;
+ROLLBACK TO SAVEPOINT savepoint_01;
+UPDATE animals
+SET weight_kg = weight_kg*-1
+WHERE weight_kg < 0;
+COMMIT;
+
+
+
